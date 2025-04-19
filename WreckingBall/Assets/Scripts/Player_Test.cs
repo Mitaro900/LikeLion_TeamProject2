daaab088fe612cc221763711685fb97557ddb717
Mitaro900 <jhkim9622@gmail.com>
2025-04-19 22:10:47 +0900

    플레이어 테스트

--- WreckingBall/Assets/Scripts/Player_Test.cs
+++ WreckingBall/Assets/Scripts/Player_Test.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 
-public class Player_Test : MonoBehaviour
+public class Player_Test : PhysicsObject
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed = 3f;
+    private float xInput;
 
-    void Update()
+    protected override void ComputeVelocity()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            xInput = 0f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            xInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            xInput = -1f;
+        }
+        else
+        {
+            xInput = 0f;
+        }
+
+        targetVelocity.x = xInput * moveSpeed;
     }
 }
