b6272bdb6afd29bd4d49a22585675f51bb1a0149
HSangJin <sdit111@gmail.com>
2025-04-23 02:26:50 +0900

    양방향트랩 완성

---
+++ WreckingBall/Assets/Scripts/Trap/StraightMovingTrap.cs
@@ -0,0 +1,127 @@
+using PKR;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class StraightMovingTrap : TrapBase
+{
+    public enum MoveTrapState { Idle, Rushing, Returning }
+    public MoveTrapState moveState { get; private set; }
+
+    [SerializeField] private bool isHorizontalMoved;
+    [SerializeField] private bool isVerticalMoved;
+
+    [SerializeField] private bool isAutoMove = false;
+
+    private Vector3 oriPos;
+    private Player player;
+    private Vector2 movingDir;
+
+    public StraightMovingTrap(TrapProperty property) : base(property)
+    {
+        base.prop = property;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        oriPos = transform.position;
+        player = FindFirstObjectByType<Player>();
+    }
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        if (moveState == MoveTrapState.Idle)
+        {
+            if (isAutoMove && IsLookPlayer())
+            {
+                movingDir = PlayerDirection();
+                pos += (Vector3) movingDir * prop.moveSpeed * Time.deltaTime;
+                transform.position = pos;
+                moveState = MoveTrapState.Rushing;
+            }
+        }
+        else if(moveState == MoveTrapState.Rushing)
+        {
+            if (Vector2.Distance(pos, oriPos) >= prop.moveDistance)
+            {
+                moveState = MoveTrapState.Returning;
+                movingDir *= -1;
+            }
+            else
+            {
+                pos += (Vector3)(movingDir * prop.moveSpeed * Time.deltaTime);
+                transform.position = pos;
+            }
+        }
+        else if(moveState == MoveTrapState.Returning)
+        {
+            if(Vector2.Distance(transform.position, oriPos) < prop.returnSpeed * Time.deltaTime)
+            {
+                pos = oriPos;
+                transform.position = pos;
+                moveState = MoveTrapState.Idle;
+                prop.state = new() { TrapProperty.TrapState.Idle };
+            }
+            else
+            {
+                pos += (Vector3)(movingDir * prop.returnSpeed * Time.deltaTime);
+                transform.position = pos;
+            }
+        }
+    }
+
+    public void MoveCall(Vector2 dir)
+    {
+        movingDir = dir;
+        moveState = MoveTrapState.Rushing;
+    }
+
+    private bool IsLookPlayer()
+    {
+        Vector2 distance = player.transform.position - oriPos;
+        Debug.Log(new Vector2(Mathf.Abs(distance.x), Mathf.Abs(distance.y)) + " / "+new Vector2(prop.thisSize.x, prop.thisSize.y));
+        if(isHorizontalMoved)
+        {
+            if(Mathf.Abs(distance.x) < prop.moveDistance && Mathf.Abs(distance.y) < Mathf.Abs(prop.thisSize.y / 2f))
+                return true;
+        }
+        
+        if(isVerticalMoved)
+        {
+            if (Mathf.Abs(distance.y) < prop.moveDistance && Mathf.Abs(distance.x) < Mathf.Abs(prop.thisSize.x / 2f))
+                return true;
+        }
+        
+        return false;
+    }
+
+    private Vector2 PlayerDirection()
+    {
+        Vector2 v2 = player.transform.position - oriPos;
+        //좌우 이동
+        if (isHorizontalMoved && Mathf.Abs(v2.x) < prop.moveDistance && Mathf.Abs(v2.y) < Mathf.Abs(prop.thisSize.y / 2f))
+            return new Vector2(v2.x > 0f ? 1 : -1, 0);
+        //상하 이동
+        else if (isVerticalMoved && Mathf.Abs(v2.y) < prop.moveDistance && Mathf.Abs(v2.x) < Mathf.Abs(prop.thisSize.x / 2f))
+            return new Vector2(0, v2.y > 0f ? 1 : -1);
+        //이동X
+        else
+            return Vector2.zero;
+    }
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null) Debug.Log(collision.gameObject.name);
+
+        base.OnTriggerEnter2D(collision);
+
+        if(collision.gameObject.name == "Ground")
+        {
+            moveState = MoveTrapState.Returning;
+            movingDir *= -1;
+        }
+    }
+}
--- WreckingBall/Assets/Scripts/Trap/TrapBase.cs
+++ WreckingBall/Assets/Scripts/Trap/TrapBase.cs
@@ -1,50 +1,82 @@
+using PKR;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
-public struct TrapState
+public struct TrapProperty
 {
-    public enum MoveState { None, Idle, Move, Destroyable, Invincibility, Destroy }
-    public List<MoveState> state;
-    public int[] trapDir;
+    public enum TrapState { None, Idle, Move, Destroyable, Invincibility, Destroy }
+    public List<TrapState> state;
+    public Vector2[] trapDir;
     public float moveSpeed;
+    public float returnSpeed;
+    public float moveDistance;
+    public Vector2 thisSize;
 
-    public TrapState(List<MoveState> state, int[] trapDir, float moveSpeed)
+    public TrapProperty(List<TrapState> state, Vector2[] trapDir, float moveSpeed, float returnSpeed, float moveDistance, Vector2 trapSize)
     {
         this.state = state;
         this.trapDir = trapDir;
         this.moveSpeed = moveSpeed;
+        this.returnSpeed = returnSpeed;
+        this.moveDistance = moveDistance;
+        this.thisSize = trapSize;
     }
 }
 
 [System.Serializable]
 public class TrapBase : MonoBehaviour
 {
-    Animator anim; Rigidbody2D rb;
-    TrapState state;
+    protected Animator anim; protected Rigidbody2D rb;
+    [SerializeField] protected TrapProperty prop;
+    [SerializeField] private bool isActive;
+    
 
-    private void Start()
+    public TrapBase(TrapProperty property)
+    {
+        this.prop = property;
+    }
+
+    public virtual void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        if(anim != null)
+            anim.enabled = isActive;
     }
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if(!collision.GetComponent<Player>())
+        if(collision.GetComponent<Player>() != null)
         {
             Player player = collision.GetComponent<Player>();
-            if (state.state.Contains(TrapState.MoveState.Destroy))
+            Rigidbody2D _rb = player.GetComponent<Rigidbody2D>();
+            // 함정 오브젝트가 부숴지는 애니메이션 재생중일때
+            if (prop.state.Contains(TrapProperty.TrapState.Destroy))
                 return;
-            else if (state.trapDir.ToList().Contains(player.facingDir * -1))
+            //플레이어와 가시부분 충돌
+            else if (prop.trapDir != default)
             {
-                player.Damage();
+                bool isCrushed = false;
+                for(int i = 0; i < prop.trapDir.Length; i++)
+                {
+                    isCrushed = prop.trapDir[i].x == (rb.linearVelocityX > 0 ? 1 : -1);
+                    if (isCrushed) break;
+                    isCrushed = prop.trapDir[i].y == (rb.linearVelocityY > 0 ? 1 : -1);
+                    if (isCrushed) break;
+                }
+
+                if(isCrushed)
+                {
+                    //player.Damage();
+                }
             }
-            else if (state.state.Contains(TrapState.MoveState.Destroyable))
+            // 함정 오브젝트와 가시가 없는 부분과 충돌했을때 부숴지는 애니메이션 재생
+            else if (prop.state.Contains(TrapProperty.TrapState.Destroyable))
             {
                 anim.SetBool("Destroy", true);
-                state.state = new() { TrapState.MoveState.Destroy };
+                prop.state = new() { TrapProperty.TrapState.Destroy };
             }
         }
     }
@@ -56,5 +88,12 @@ public class TrapBase : MonoBehaviour
             gameObject.SetActive(false);
     }
 
-    private void OnDisable() => TrapPoolManager.instance?.Rerurn(gameObject);
+    public virtual void OnDisable() => TrapPoolManager.instance?.Rerurn(gameObject);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(new Vector3(transform.position.x - prop.thisSize.x / 2f, transform.position.y), new Vector3(prop.thisSize.x / 2f + transform.position.x, transform.position.y));
+        Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y - prop.thisSize.y / 2f), new Vector3(transform.position.x, prop.thisSize.y / 2f + transform.position.y));
+    }
 }
