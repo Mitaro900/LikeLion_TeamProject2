4ba94884c94c8adcfe29668bb9420380cd72bcb2
JWooY <jettyy4@naver.com>
2025-05-09 18:08:38 +0900

    지상 몹 수정 및 사망 / 스턴 추가
    기존 코드와 호환되도록 전체적으로 수정
    Mush/BushBeast 사망/스턴 기능 추가
    (수정 및 보완 필요)
    Leafrim 미완

---
+++ WreckingBall/Assets/Scripts/Entities/Enemies/GroundEnemy/EnemyLeafrim/LeafrimAttackState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class LeafrimAttackState : State
+{
+    private Enemy_Leafrim enemy;
+
+    public LeafrimAttackState(Entity entity, StateMachine stateMachine, string animBoolName, Enemy_Leafrim enemy) : base(entity, stateMachine, animBoolName)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        enemy.moveSpeed += 2f;
+        enemy.Radius += 3f;
+        enemy.PlayerCheckRadius += 3f;
+
+        stateTimer = enemy.AttackCooldown;
+    }
+    public override void Update()
+    {
+        base.Update();
+
+
+        enemy.SetZeroVelocity();
+
+        if (triggerCalled || stateTimer < 0f)
+        {
+            stateMachine.ChangeState(enemy.battleState);
+            return;
+        }
+
+    }
+    public override void Exit()
+    {
+        base.Exit();
+        enemy.lastTimeAttacked = Time.time;
+        enemy.moveSpeed -= 2f;
+        enemy.Radius -= 3f;
+        enemy.PlayerCheckRadius -= 3f;
+    }
+
+
+}
